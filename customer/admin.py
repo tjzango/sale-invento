4c4fd852cea524ee91d2d149d56651aa64e24a7b
tjzango <tjzango@gmail.com>
2018-08-05 20:49:10 +0100

    Registered all models

--- customer/admin.py
+++ customer/admin.py
@@ -4,7 +4,10 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 from customer import models
+from  cart.models import Item, ItemManager, Cart
 
 
 # Register your models here.
 admin.site.register(models.Customer)
+admin.site.register(Item)
+admin.site.register(Cart)
