3164963fd307bb94ecd6a3789e92b98bc0f58f6f
tjzango <tjzango@gmail.com>
2018-08-05 20:47:44 +0100

    Configure urls

--- sale/urls.py
+++ sale/urls.py
@@ -4,9 +4,21 @@ from django.conf.urls import url
 
 from sale.views import (
     index,
+    new_sale,
+    remove_item,
+    increase_quantity,
+    decrease_quantity,
+    add_item_to_cart,
+    clear_cart,
 )
 
 # this are the url patterns their name and vue there pointing to
 urlpatterns = [
     url(r'^$', index, name='index'),
+    url(r'^new/$', new_sale, name='new_sale'),
+    url(r'^new/cart/(?P<key>[0-9]+)/(?P<quantity>[0-9]+)/-/$', decrease_quantity, name='cart_decrease'),
+    url(r'^new/cart/(?P<key>[0-9]+)/(?P<quantity>[0-9]+)/+/$', increase_quantity, name='cart_increase'),
+    url(r'^new/cart/(?P<key>[0-9]+)$', add_item_to_cart, name='cart'),
+    url(r'^new/cart/clear/$', clear_cart, name='clear_cart'),
+    url(r'^new/cart/clear/(?P<key>[0-9]+)/$', remove_item, name='remove_item'),
 ]
