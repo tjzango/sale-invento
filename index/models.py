624d4b14328635b42ad571be172772ecde9ba40a
tjzango <tjzango@gmail.com>
2018-08-13 07:25:14 +0100

    Updated Account structure

--- index/models.py
+++ index/models.py
@@ -15,22 +15,21 @@ LEVEL = (
     ('Store Keeper', 'Store Keeper')
 )
 
-
+EMPLOYEE_LEVEL = (
+    ('Manager', 'Manager'),
+    ('Cleaner', 'Cleaner'),
+    ('Cashier', 'Cashier'),
+    ('Store Keeper', 'Store Keeper'),
+    ('Other', 'Other')
+)
 # Create your models here.
 # Users account defination e.g muslim has attribute user, contact and level
 # user has a forignkey to the User(first_name, last_name, password, username and email) models
 # contact is a VARCHAR Field
 # level is contain either three values 1,2 or 3 i.e Administrator, cashier or storekeeper respectively
-class Account(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    contact = models.CharField(max_length=20)
-    level = models.CharField(max_length=20, choices=LEVEL)
-
-    def __str__(self):
-        return self.user.username
-
-
 # Employee Defination
+
+
 class Employee(models.Model):
     name = models.CharField(max_length=100)
     address = models.TextField()
@@ -38,7 +37,16 @@ class Employee(models.Model):
     status = models.CharField(choices=STATUS, max_length=20)
     salary = models.PositiveIntegerField()
     joined_on = models.DateField()
-    level = models.PositiveIntegerField()
+    level = models.CharField(max_length=20, choices=EMPLOYEE_LEVEL)
+    contact = models.CharField(max_length=20, default='1')
 
     def __str__(self):
         return self.name
+
+
+class Account(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    employee = models.ForeignKey(Employee)
+
+    def __str__(self):
+        return self.user.username
